58f995de9115e2ff9f8ed6a47c34fe097d4cc4cb
Hxrsha-x <ahkharsha@gmail.com>
2024-09-29 02:32:51 +0530

    Hardhattt and smart contracts

---
+++ contract/hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    // hardhat: {
+    //   chainId: 31337,
+    // },
+  },
+};
