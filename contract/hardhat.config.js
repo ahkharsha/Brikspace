afc1072eec2577c1c91c708dda81c0f961761896
Hxrsha-x <ahkharsha@gmail.com>
2024-09-29 12:56:53 +0530

    Fix smart contracts

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -16,5 +16,9 @@ module.exports = {
     // hardhat: {
     //   chainId: 31337,
     // },
+    polygon_amoy: {
+      url: process.env.NEXT_PUBLIC_POLYGON_AMOY_RPC,
+      accounts: [`0x${process.env.NEXT_PUBLIC_PRIVATE_KEY}`],
+    },
   },
 };
